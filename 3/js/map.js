ca75308e22cc0a951dd0bdf38d30f59f7d957ae2
Keks <keks@htmlacademy.ru>
2018-06-12 22:14:53 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/map.js
+++ 3/js/map.js
@@ -1,11 +1,11 @@
 'use strict';
 
 /*
-task1Function - создаёт массив объявлений с рандомными параметрами
-task2Function - убирает у заданного класса заданную метку (врЕменная функция)
-task3Function - создает из массива объявлений массив из ДОМ элементов (массив для меток на карте)
-task4Function - добавляет массив из ДОМ элементов (меток на карте) в разметку. Затем отображает
-task5Function - создаёт и отображает развернутую информацию по какому то одному объявлению
+task1 - создаёт массив объявлений с рандомными параметрами
+task2 - убирает у заданного класса заданную метку (врЕменная функция)
+task3 - создает из массива объявлений массив из ДОМ элементов (массив для меток на карте)
+task4 - добавляет массив из ДОМ элементов (меток на карте) в разметку. Затем отображает
+task5 - создаёт и отображает развернутую информацию по какому то одному объявлению
 setAppartmentsPhotos - добавляет шаблон внутри шаблона. Добавляемый шаблон формируется из набора тегов описывающих фото апартаментов
 */
 
@@ -50,67 +50,69 @@ var FEATURES = [
   'elevator',
   'conditioner'
 ];
-var randomGenerator = {
-  modeGenerator: {
-    hard: true,
-    soft: false
-  },
-  getRandomValue: function (minValue, maxValue, mode) {
-    var randomValue = maxValue - (maxValue - 1 - minValue) * Math.random();
-    return mode ? Math.round(randomValue) : Math.floor(randomValue);
-  }
+
+var getRandomValue = function (minValue, maxValue) {
+  var randomValue = maxValue - (maxValue - 1 - minValue) * Math.random();
+  return Math.round(randomValue);
+};
+
+var getRandomArrayElement = function (array) {
+  var maxValue = array.length - 1;
+  return array[getRandomValue(0, maxValue)];
 };
 
 var getFeatures = function () {
   var features = [];
-  var genMode = randomGenerator.modeGenerator;
-  var howManyFeatures = randomGenerator.getRandomValue(0, FEATURES.length, genMode.soft);
-  var temp = randomGenerator.getRandomValue(0, FEATURES.length, genMode.soft);
+  var maxValue = FEATURES.length - 1;
+  var howManyFeatures = getRandomValue(0, maxValue);
+  var temp = getRandomValue(0, maxValue);
   for (var i = 0; i < howManyFeatures; ++i) {
     features.push(FEATURES[(temp + i) % FEATURES.length]);
   }
   return features;
 };
 
-var task1Function = function (adsQuantity) {
+var Ad = function (index) {
+  this.author = {};
+  this.author.avatar = 'img/avatars/user0' + (index + 1) + '.png';
+  this.offer = {};
+  var offer = this.offer;
+  offer.title = getRandomArrayElement(TITLE);
+  offer.address = getRandomValue(MIN_X_LOCATION, MAX_X_LOCATION) + ', ' + getRandomValue(MIN_Y_LOCATION, MAX_Y_LOCATION);
+  offer.price = getRandomValue(MIN_PRICE, MAX_PRICE);
+  offer.type = getRandomArrayElement(Object.keys(TYPE));
+  offer.rooms = getRandomValue(MIN_ROOMS, MAX_ROOMS);
+  offer.guests = getRandomValue(MIN_GUESTS, MAX_GUESTS);
+  offer.checkin = getRandomArrayElement(CHECKPOINT);
+  offer.checkout = getRandomArrayElement(CHECKPOINT);
+  offer.features = getFeatures();
+  offer.description = ' ';
+  offer.photos = [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  ];
+  this.location = {};
+  this.location.x = getRandomValue(MIN_X_LOCATION, MAX_X_LOCATION);
+  this.location.y = getRandomValue(MIN_Y_LOCATION, MAX_Y_LOCATION);
+};
+
+var createAds = function (adsQuantity) {
   var ads = [];
-  var genMode = randomGenerator.modeGenerator;
   for (var i = 0; i < adsQuantity; ++i) {
-    var ad = {};
-    ad.author = {};
-    ad.author.avatar = 'img/avatars/user0' + (i + 1) + '.png';
-    ad.offer = {};
-    var offer = ad.offer;
-    offer.title = TITLE[randomGenerator.getRandomValue(0, TITLE.length, genMode.soft)];
-    offer.address = randomGenerator.getRandomValue(MIN_X_LOCATION, MAX_X_LOCATION, genMode.hard) + ', ' + randomGenerator.getRandomValue(MIN_Y_LOCATION, MAX_Y_LOCATION, genMode.hard);
-    offer.price = randomGenerator.getRandomValue(MIN_PRICE, MAX_PRICE, genMode.hard);
-    offer.type = Object.keys(TYPE)[randomGenerator.getRandomValue(0, TYPE.length, genMode.soft)];
-    offer.rooms = randomGenerator.getRandomValue(MIN_ROOMS, MAX_ROOMS, genMode.hard);
-    offer.guests = randomGenerator.getRandomValue(MIN_GUESTS, MAX_GUESTS, genMode.hard);
-    offer.checkin = CHECKPOINT[randomGenerator.getRandomValue(0, CHECKPOINT.length, genMode.soft)];
-    offer.checkout = CHECKPOINT[randomGenerator.getRandomValue(0, CHECKPOINT.length, genMode.soft)];
-    offer.features = getFeatures();
-    offer.description = ' ';
-    offer.photos = [
-      'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-      'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-      'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-    ];
-    ad.location = {};
-    ad.location.x = randomGenerator.getRandomValue(MIN_X_LOCATION, MAX_X_LOCATION, genMode.hard);
-    ad.location.y = randomGenerator.getRandomValue(MIN_Y_LOCATION, MAX_Y_LOCATION, genMode.hard);
+    var ad = new Ad(i);
     ads.push(ad);
   }
   return ads;
 };
 
 // task-2
-var task2Function = function () {
+var renderBlockMap = function () {
   document.querySelector('.map').classList.remove('map--faded');
 };
 
 // task-3
-var getIconsGeometry = function (iconDomElement) {
+var getIconGeometry = function (iconDomElement) {
   var iconGeometry = {
     x: iconDomElement.width,
     y: iconDomElement.height
@@ -118,12 +120,12 @@ var getIconsGeometry = function (iconDomElement) {
   return iconGeometry;
 };
 
-var task3Function = function (ads) {
+var createPointElements = function (ads) {
   var template = document.querySelector('template').content.querySelector('.map__pin');
   var points = [];
   // get icons geometry parameters
   var icon = document.querySelector('template').content.querySelector('.map__pin').querySelector('img');
-  var iconGeometry = getIconsGeometry(icon);
+  var iconGeometry = getIconGeometry(icon);
   for (var i = 0; i < ADS_QUANTITY; ++i) {
     points[i] = template.cloneNode(true);
     var xCoorLocation = ads[i].location.x - parseInt(iconGeometry.x, 10) / 2;
@@ -136,7 +138,7 @@ var task3Function = function (ads) {
 };
 
 // task-4
-var task4Function = function (points) {
+var renderPointsBlock = function (points) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < points.length; ++i) {
     fragment.appendChild(points[i]);
@@ -161,7 +163,7 @@ var setAppartmentsPhotoes = function (card, templatePhoto, photos) {
   photoDiv.appendChild(fragment);
 };
 
-var task5Function = function (ad) {
+var renderAd = function (ad) {
   var templateCard = document.querySelector('template').content.querySelector('.map__card');
   var templatePhoto = document.querySelector('template').content.querySelector('.popup__photo');
   var card = templateCard.cloneNode(true);
@@ -180,8 +182,8 @@ var task5Function = function (ad) {
 };
 
 // использование
-var ads = task1Function(ADS_QUANTITY);
-task2Function();
-var points = task3Function(ads);
-task4Function(points);
-task5Function(ads[0]);
+var ads = createAds(ADS_QUANTITY);
+renderBlockMap();
+var points = createPointElements(ads);
+renderPointsBlock(points);
+renderAd(ads[0]);
