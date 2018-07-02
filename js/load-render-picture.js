207cc3df8afd79aab1c5bdf9b79054f2dc0bbcf3
Oleg <olegsemerkin777@gmail.com>
2018-07-02 16:02:50 +0300

    Update load-render-picture.js

--- js/load-render-picture.js
+++ js/load-render-picture.js
@@ -3,28 +3,23 @@
 (function () {
   var elementDefaultAvatarPhoto = document.querySelector('.ad-form-header__preview').querySelector('img');
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-  var ImageAvatar = function (source) {
-    this.width = 70;
-    this.height = 70;
+  var Image = function (config, source) {
+    this.width = config.width;
+    this.height = config.height;
     this.src = source;
-    this.alt = 'Фотография пользователя';
+    this.alt = config.alt;
   };
-  var ImageApartments = function (source) {
-    this.width = 70;
-    this.height = 70;
-    this.src = source;
-    this.alt = 'Фотография апартаментов';
-  };
-  var toNode = function (image) {
+  Image.prototype.toNode = function () {
     var elementImage = document.createElement('img');
-    Object.keys(image).forEach(function (key) {
-      elementImage[key] = image[key];
+    var self = this;
+    Object.keys(this).forEach(function (key) {
+      elementImage[key] = self[key];
     });
     return elementImage;
   };
   var renderer = {
     renderPhoto: function (source) {
-      var elementImage = toNode(new this.renderParameters.constructor(source));
+      var elementImage = (new Image(this.renderParameters.config, source)).toNode();
       if (isAnyPhotoes(this.renderParameters.container.node) && !canBeMorePhotoes(this.renderParameters.container)) {
         renderer.deletePhotoes.call(this.renderParameters.container.node);
       }
@@ -37,20 +32,28 @@
       });
     }
   };
-  var idInputFileToRenderParameters = {
+  var idInputFileToRenderParameter = {
     images: {
       container: {
         node: document.querySelector('.ad-form__photo'),
         moreThanOnePhoto: true
       },
-      constructor: ImageApartments
+      config: {
+        width: 70,
+        height: 70,
+        alt: 'Фотография апартаментов'
+      }
     },
     avatar: {
       container: {
         node: document.querySelector('.ad-form-header__preview'),
         moreThanOnePhoto: false
       },
-      constructor: ImageAvatar
+      config: {
+        width: 70,
+        height: 70,
+        alt: 'Фотография пользователя'
+      }
     }
   };
   var isAnyPhotoes = function (elementContainer) {
@@ -62,7 +65,7 @@
 
   var onChanged = function (evt) {
     var elementInput = evt.target;
-    elementInput.renderParameters = idInputFileToRenderParameters[elementInput.id];
+    elementInput.renderParameters = idInputFileToRenderParameter[elementInput.id];
     Array.prototype.forEach.call(elementInput.files, function (file) {
       var fileName = file.name.toLowerCase();
       var match = FILE_TYPES.some(function (typeName) {
@@ -93,8 +96,8 @@
 
   window.loadRenderPictures = {};
   window.loadRenderPictures.deletePhotoes = function () {
-    Object.keys(idInputFileToRenderParameters).forEach(function (key) {
-      var container = idInputFileToRenderParameters[key].container.node;
+    Object.keys(idInputFileToRenderParameter).forEach(function (key) {
+      var container = idInputFileToRenderParameter[key].container.node;
       if (isAnyPhotoes(container)) {
         renderer.deletePhotoes.call(container);
       }
