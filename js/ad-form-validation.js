4712660fd19da6c9510dfc26496902df5c9a6afc
Oleg <olegsemerkin777@gmail.com>
2018-07-02 16:01:20 +0300

    Update ad-form-validation.js

--- js/ad-form-validation.js
+++ js/ad-form-validation.js
@@ -5,29 +5,29 @@
 (function () {
 
   var validityTypeToNotice = {
-    valueMissing: function () {
+    getValueMissingNotice: function () {
       return 'This field is must being filled';
     },
-    typeMismatch: function () {
+    getTypeMismatchNotice: function () {
       return 'Input correct ' + this.type + '!';
     },
-    tooShort: function () {
+    getTooShortNotice: function () {
       return 'Input value is too short. Minimum length is ' + this.minLength + ' characters!';
     },
-    tooLong: function () {
+    getTooLongNotice: function () {
       return 'Input value is too long. Minimum length is ' + this.maxLength + ' characters!';
     },
-    rangeUnderflow: function () {
+    getRangeUnderflowNotice: function () {
       return 'Input value is too small. Minimum value is ' + this.min + ' roubles!';
     },
-    rangeOverflow: function () {
+    getRangeOverflowNotice: function () {
       return 'Input value is too great. Maximum value is ' + this.max + ' roubles!';
     },
-    patternMismatch: function () {
+    getPatternMismatchNotice: function () {
       return 'Input value mismatch with pattern!';
     }
   };
-  var getErrorType = function (element) {
+  var getErrorTypeName = function (element) {
     var validity = element.validity;
     var type;
     for (type in validity) {
@@ -37,9 +37,13 @@
     }
     return type;
   };
+  var getFunctionName = function (typeName) {
+    return 'get' + typeName[0].toUpperCase() + typeName.slice(1, typeName.length) + 'Notice';
+  };
   var getNotice = function (element) {
-    var type = getErrorType(element);
-    var notice = validityTypeToNotice[type].call(element);
+    var typeName = getErrorTypeName(element);
+    var functionName = getFunctionName(typeName);
+    var notice = validityTypeToNotice[functionName].call(element);
     return notice;
   };
 
